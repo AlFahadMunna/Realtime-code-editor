5edd269b9a8d69af430e0acd73dd1c5ad119cf47
AlFahadMunna <afmunna.afm.afm@gmail.com>
2024-12-02 23:51:13 +0600

    new project started

---
+++ src/pages/EditorPage.jsx
@@ -0,0 +1,5 @@
+const EditorPage = () => {
+  return <div>Editot Page</div>;
+};
+
+export default EditorPage;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">form</div>
+    </div>
+  );
+};
+
+export default Home;
